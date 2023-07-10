576d0707b0b93edbc93b1f2bcfb72753d0f6b0d8
felipeJJ <jorgefelipe11@hotmail.com>
2023-07-10 18:38:14 -0300

    refactor: displaing a title

--- pages/produtos/novo.tsx
+++ pages/produtos/novo.tsx
@@ -1,10 +1,11 @@
 import Form from "@/components/Form";
 import Layout from "@/components/Layout";
 
-export default function novoProduto() {
+export default function NovoProduto() {
   return (
     <Layout>
+      <h1>Crie novos produtos!</h1>  
       <Form />
     </Layout>
   )
-}
+} 
