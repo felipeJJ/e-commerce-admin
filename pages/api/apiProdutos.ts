dc938175d420e0fa42899a8fed65860babe7b9fc
felipeJJ <jorgefelipe11@hotmail.com>
2023-07-10 18:34:13 -0300

    feat: created api methods GET and POST

--- pages/api/apiProdutos.ts
+++ pages/api/apiProdutos.ts
@@ -1,19 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
-import produtoController from "../../databese/controllers/ProdutoController";
 import database from "@/databese/lib/mongoose";
 import Produto from "@/databese/schemas/produto";
 
-/* export default async function handlerApi(req: any, res: any) {
-  await produtoController.postProduto(req.body, res.body)
-  
-  res.status(200).json({message: 'Produto created successfully'})
-}
 
- */
-export default async function handlerApi(req: any, res: any){
+export default async function handlerApi(req: any, res:any){
+
   const {method} = req
   await database.connectMongo()
-  if (method === 'POST'){
+
+  if (method === 'GET') {
+    if (req.query?.id) {
+      res.json(await Produto.findOne({_id:req.query.id}));
+    } else {
+      res.json(await Produto.find());
+    }
+  }
+
+  else if (method === 'POST'){
     const {nome, descricao, preco} = req.body
     const produtoDoc = await Produto.create({
       nome,descricao,preco
