95d591da7a1d32a765da8fea2de570d4a6ef7cef
felipeJJ <jorgefelipe11@hotmail.com>
2024-07-20 19:03:19 -0300

    fix: Type error with contentType

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -6,39 +6,38 @@ import mime from 'mime-types'
 const bucketName = 'jorgim-ecommerce'
 
 export default async function Handle(req: any, res: any) {
-    const form = new multiparty.Form();
-    const {fields,files} = await new Promise<{ fields: any, files: any }>((resolve,reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      resolve({fields,files});
-    });
-  });
+    const form = new multiparty.Form()
+    const { fields, files } = await new Promise<{ fields: any, files: any }>((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) reject(err)
+            resolve({ fields, files })
+        })
+    })
     const client = new S3Client({
-    region: 'sa-east-1',
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY as string,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
-    },
-  })
-  const links = []
-  for(const file of files.file){
+        region: 'sa-east-1',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY as string,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string,
+        },
+    })
+    const links = []
+    for (const file of files.file) {
         const ext = file.originalFilename.split('.').pop()
         const newFilename = Date.now() + '.' + ext
-        console.log({ext, file})
-        client.send(new PutObjectCommand({
+        const contentType = mime.lookup(file.path) || undefined
+        await client.send(new PutObjectCommand({
             Bucket: bucketName,
             Key: newFilename,
             Body: fs.readFileSync(file.path),
             ACL: 'public-read',
-            ContentType: mime.lookup(file.path),
+            ContentType: contentType,
         }))
         const link = `https://${bucketName}.s3.sa-east-1.amazonaws.com/${newFilename}`
-        links.push(link)
+        links.push(link);
     }
-    return res.json({links})
-
+    return res.json({ links })
 }
 
 export const config = {
-    api: {bodyParser: false},
+    api: { bodyParser: false },
 }
